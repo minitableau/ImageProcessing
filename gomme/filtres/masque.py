ce27d7bb8f2a9e0169a05ebd8951c0c1adabb00c
minitableau <minitableau2002@gmail.com>
2021-03-28 20:56:36 +0200

    ajout de texte

--- gomme/filtres/masque.py
+++ gomme/filtres/masque.py
@@ -10,7 +10,11 @@ def appliquer_masque(image: ndarray, masque: ndarray):
 
     for i in range(lignes):
         for j in range(colonnes):
-            if tableau_masque[i, j] < 170:      #car les contours du masque ne sont pas totalement noir (1 seul valeur car lecture en noir|blanc)
-                image_copie[i, j] = [0, 0, 0]   #[255, 255, 255] pour blanc ou [0, 0, 0] pour noir
+            if tableau_masque[i, j] < 170:  # car les contours du masque ne sont pas totalement noir (1 seul valeur car lecture en noir|blanc)
+                image_copie[i, j] = [0, 0, 0]  # [255, 255, 255] pour blanc ou [0, 0, 0] pour noir
 
     return image_copie
+
+# On cherchera a incruster une confiance pour ensuite pouvoir repérer les pixels qui pourront être utiliser pour
+# recrée l'image
+
